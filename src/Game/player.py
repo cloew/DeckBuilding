fb7a995f01c599a655182c37d136620084948066
Chris Loew <cloew123@gmail.com>
2014-09-04 10:09:59 -0600

    Added stubbed out player class

---
+++ src/Game/player.py
@@ -0,0 +1,9 @@
+
+class Player:
+    """ Represents a Player in the Game """
+    
+    def __init__(self):
+        """ Initialize a Player """
+        self.deck = None
+        self.hand = None
+        self.ongoing = None
