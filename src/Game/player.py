345c187a9f20926fedc415fd08a8a2359a5b7929
Chris Loew <cloew123@gmail.com>
2014-09-04 11:26:38 -0600

    Player now starts with a deck and draws a card

--- src/Game/player.py
+++ src/Game/player.py
@@ -1,9 +1,12 @@
+from Game.Decks.decks import StartingDeckInitializer
+from kao_deck.deck_with_discard_pile import DeckWithDiscardPile
 
 class Player:
     """ Represents a Player in the Game """
+    HAND_SIZE = 5
     
     def __init__(self):
         """ Initialize a Player """
-        self.deck = None
-        self.hand = None
+        self.deck = DeckWithDiscardPile(deck_initializer=StartingDeckInitializer, reshuffle=True)
+        self.hand = self.deck.draw(count=self.HAND_SIZE)
         self.ongoing = None
