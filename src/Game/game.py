48121d26abb8d77f7ba4a21d5a50c1ab557f67e8
Chris Loew <cloew123@gmail.com>
2014-09-04 11:37:38 -0600

    The game can now construct its players

--- src/Game/game.py
+++ src/Game/game.py
@@ -1,10 +1,14 @@
+from player import Player
 
 class Game:
     """ Represents a game of the Deck Building Game """
     
-    def __init__(self):
+    def __init__(self, numberOfPlayers):
         """ Initialize the Game """
         self.players = []
+        for i in range(numberOfPlayers):
+            self.players.append(Player())
+            
         self.mainDeck = None
         self.lineUp = None
         self.weaknessDeck = None
