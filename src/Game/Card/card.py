1ec1f1a74f2c07676ef3ab6517eb6f65b73b2a6e
Chris Loew <cloew123@gmail.com>
2014-09-04 11:17:28 -0600

    Can now pass some parameters to the card constructor

--- src/Game/Card/card.py
+++ src/Game/Card/card.py
@@ -2,12 +2,12 @@
 class Card:
     """ Represents a Card in the Deck Building Game """
     
-    def __init__(self):
+    def __init__(self, name, playEffects=[]):
         """ Initialize the Card """
-        self.name = None
+        self.name = name
         self.costCalculator = None
         self.victoryPointsCalculator = None
         
-        self.playEffects = None
-        self.availableEffects = None
+        self.playEffects = playEffects
+        self.activatableEffects = None
         self.triggerableEffects = None
