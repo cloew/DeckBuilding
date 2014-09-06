d3c56c26af0a300e1ffc6d15c404850203a3ffb5
Chris Loew <cloew123@gmail.com>
2014-09-06 15:28:56 -0600

    Added Play Card Controller to play a specific card

---
+++ src/Server/Controller/play_card_controller.py
@@ -0,0 +1,14 @@
+from Game.Commands.play_card import PlayCard
+from Server.Game.game_wrapper import GameWrapper
+
+from kao_flask.controllers.json_controller import JSONController
+
+class PlayCardController(JSONController):
+    """ Controller to play a card """
+    
+    def performWithJSON(self, gameId):
+        game = GameWrapper(id=gameId)
+        cardIndex = self.json['index']
+        card = game.game.currentTurn.player.hand[cardIndex]
+        PlayCard(card, game.game.currentTurn).perform()
+        return GameWrapper(id=gameId).toJSON()
--- src/Server/routes.py
+++ src/Server/routes.py
@@ -1,9 +1,11 @@
 from Server.Controller.start_game_controller import StartGameController
 from Server.Controller.get_game_controller import GetGameController
+from Server.Controller.play_card_controller import PlayCardController
 
 from kao_flask.endpoint import Endpoint
 from kao_flask.controllers.html_controller import HTMLController
 
 routes = [Endpoint('/', get=HTMLController('Server/templates/index.html')),
           Endpoint('/api/startgame', post=StartGameController()),
-          Endpoint('/api/game/<int:gameId>', get=GetGameController())]
+          Endpoint('/api/game/<int:gameId>', get=GetGameController()),
+          Endpoint('/api/game/<int:gameId>/play', post=PlayCardController())]
