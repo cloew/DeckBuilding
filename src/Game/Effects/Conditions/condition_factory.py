b1bab6f743532ab0a77f6675f87d84c7a82be752
Chris Loew <cloew123@gmail.com>
2014-09-07 16:53:53 -0600

    Added Condition Factory to construct the Conditions

---
+++ src/Game/Effects/Conditions/condition_factory.py
@@ -0,0 +1,10 @@
+from matching import Matching
+
+class ConditionFactory:
+    """ Factory to build Conditions """
+    
+    def loadCondition(self, conditionJSON):
+        """ Load the Condition from the given JSON """
+        if conditionJSON["type"] == "MATCHING":
+            return Matching(conditionJSON["field"], conditionJSON["values"], conditionJSON["sourceType"])
+        return None
