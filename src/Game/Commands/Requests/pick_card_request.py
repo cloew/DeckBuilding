0fcad22edebc2395671a0c0a346c74f246bf5484
Chris Loew <cloew123@gmail.com>
2014-09-15 12:11:08 -0600

    Added Request for Picking Cards

---
+++ src/Game/Commands/Requests/pick_card_request.py
@@ -0,0 +1,7 @@
+
+class PickCardRequest:
+    """ Represents a Request to Pick Cards """
+    
+    def __init__(self, cards):
+        """ Initialize the Request with the potential options """
+        self.cards = cards
