caf736222382d111a53e4c082fc2af5a200bf1d2
Chris Loew <cloew123@gmail.com>
2014-09-16 09:56:04 -0600

    Look At Top Effect now automatically ignroes itself if there is no top card

--- src/Game/Effects/look_at_top.py
+++ src/Game/Effects/look_at_top.py
@@ -1,4 +1,5 @@
 from Game.Effects.effect_runner import PerformEffect
+from Game.Effects.Conditions.has_cards import HasCards
 from Game.Events.top_card_event import TopCardEvent
 from Game.Sources.source_factory import SourceFactory
 
@@ -9,13 +10,15 @@ class LookAtTop:
         """ Initialize the Effect with the source to look at """
         self.sourceType = sourceType
         self.thenEffect = thenEffect
+        self.condition = HasCards(self.sourceType)
         
     def perform(self, args):
         """ Perform the Game Effect """
-        source = SourceFactory.getSource(self.sourceType, args.game, event=args.event)
-        card = source[0]
-        event = TopCardEvent(card, source, args.game)
-        coroutine = PerformEffect(self.thenEffect, event.args)
-        response = yield coroutine.next()
-        while True:
-            response = yield coroutine.send(response)
+        if self.condition.evaluate(args.game, event=args.event):
+            source = SourceFactory.getSource(self.sourceType, args.game, event=args.event)
+            card = source[0]
+            event = TopCardEvent(card, source, args.game)
+            coroutine = PerformEffect(self.thenEffect, event.args)
+            response = yield coroutine.next()
+            while True:
+                response = yield coroutine.send(response)
