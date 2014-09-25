ab3225237ec5d42dec26cf3bb973b45232c4a512
Chris Loew <cloew123@gmail.com>
2014-09-25 11:08:19 -0600

    Added operation to check if a card is even or odd

--- src/Game/Effects/Conditions/Filters/Operations/operations.py
+++ src/Game/Effects/Conditions/Filters/Operations/operations.py
@@ -2,8 +2,16 @@ import operator
 
 def inOperator(value, container):
     return operator.contains(container, value)
+    
+def compareEvenOrOdd(value, expected):
+    return self.getEvenOrOdd(value) == expected
+    
+def getEvenOrOdd(self, value):
+    options = {0:"EVEN", 1:"ODD"}
+    return options[value % 2]
 
 operations = {"==":operator.eq,
               ">=":operator.ge,
               "<=":operator.le,
-              "IN":inOperator}
+              "IN":inOperator,
+              "EVEN_OR_ODD":compareEvenOrOdd}
