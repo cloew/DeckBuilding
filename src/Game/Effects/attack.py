e969f3dfdd72d9096b603c8862d0dccd1e4f5288
Chris Loew <cloew123@gmail.com>
2014-09-26 14:33:53 -0600

    Added Per Foe Effect to be used as the background effect of the Attack foe ability

--- src/Game/Effects/attack.py
+++ src/Game/Effects/attack.py
@@ -1,25 +1,8 @@
-from Game.Effects.effect_runner import PerformEffect
+from Game.Effects.per_foe import PerFoe
 
-import copy
-
-class Attack:
+class Attack(PerFoe):
     """ Represents an effect to Attack other Players """
     
     def __init__(self, effect):
-        """ Initialize the Effect with the effect to attack with """
-        self.effect = effect
-        
-    def perform(self, args):
-        """ Perform the Game Effect """
-        foes = args.foes
-        for foe in args.foes:
-            args = copy.copy(args)
-            args.player = foe
-            
-            coroutine = PerformEffect(self.effect, args)
-            try:
-                response = yield coroutine.next()
-                while True:
-                    response = yield coroutine.send(response)
-            except StopIteration:
-                pass
+        """ Initialize with the effect to attack with """
+        PerFoe.__init__(self, [effect])
---
+++ src/Game/Effects/per_foe.py
@@ -0,0 +1,25 @@
+from Game.Effects.effect_runner import PerformEffects
+
+import copy
+
+class PerFoe:
+    """ Represents an effect to perform for each foe """
+    
+    def __init__(self, effects):
+        """ Initialize the Effect with the effect to attack with """
+        self.effects = effects
+        
+    def perform(self, args):
+        """ Perform the Game Effect """
+        foes = args.foes
+        for foe in args.foes:
+            args = copy.copy(args)
+            args.player = foe
+            
+            coroutine = PerformEffects(self.effects, args)
+            try:
+                response = yield coroutine.next()
+                while True:
+                    response = yield coroutine.send(response)
+            except StopIteration:
+                pass
