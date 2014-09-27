c4b2320ec978b63a9f16b0a97f7d28fa74ca1c94
Chris Loew <cloew123@gmail.com>
2014-09-26 21:48:22 -0600

    Removed debug statements

--- src/Game/power_tracker.py
+++ src/Game/power_tracker.py
@@ -10,7 +10,6 @@ class PowerTracker:
         
     def gainPower(self, card, power):
         """ Gain Power from the given card """
-        print "Gaining Power:", power
         if card in self.cardToPower:
             self.cardToPower[card] += power
         else:
@@ -23,7 +22,4 @@ class PowerTracker:
     @property
     def power(self):
         """ Return the current power level for the turn """
-        print "Gained Power:", sum(self.cardToPower.values())
-        print "Times Modifier:", sum(self.cardToPower.values())*self.modifier
-        print "Minus Spent Power:", sum(self.cardToPower.values())*self.modifier - self.spentPower
         return sum(self.cardToPower.values())*self.modifier - self.spentPower
