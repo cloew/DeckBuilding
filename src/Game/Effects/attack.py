46280281a9436b605b057ac136e5d9ab1119419d
Chris Loew <cloew123@gmail.com>
2014-09-30 10:36:54 -0600

    Added the ability to call Defense Effects from the Attack

--- src/Game/Effects/attack.py
+++ src/Game/Effects/attack.py
@@ -1,5 +1,7 @@
 from Game.Commands.Requests.defend_request import DefendRequest
-from Game.Effects.effect_runner import PerformEffectsForEachPlayer
+from Game.Effects.effect_runner import PerformEffectsForEachPlayer, PerformEffects
+from Game.Events.cards_event import CardsEvent
+from Game.Sources.source_factory import SourceFactory, HAND
 
 class Attack:
     """ Represents an effect to Attack other Players """
@@ -16,7 +18,18 @@ class Attack:
             if not defended:
                 targets.append(foe)
             else:
-                pass # Perform the Defend Effects
+                playerArgs = args.copy()
+                playerArgs.player = foe
+                source = SourceFactory.getSourceForEffect(HAND, playerArgs)
+                event = CardsEvent([defended], source, playerArgs)
+                
+                coroutine = PerformEffects(defended.defenseEffects, event.args)
+                try:
+                    response = yield coroutine.next()
+                    while True:
+                        response = yield coroutine.send(response)
+                except StopIteration:
+                    pass
                 
         coroutine = PerformEffectsForEachPlayer(self.effects, targets, args)
         response = yield coroutine.next()
