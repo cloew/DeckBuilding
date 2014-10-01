9cefdbf10c444d8db044a5a8239333158fd2cb02
Chris Loew <cloew123@gmail.com>
2014-10-01 10:19:42 -0600

    The Character Image now flips if the character is deactivated

--- src/Server/Json/character_wrapper.py
+++ src/Server/Json/character_wrapper.py
@@ -10,4 +10,5 @@ class CharacterWrapper:
     def toJSON(self):
         """ Return the card as a JSON Dictionary """
         return {'image':self.CHARACTER_IMAGES_DIRECTORY_URL+self.character.image,
-                'name':self.character.name}
+                'name':self.character.name,
+                'active':self.character.active}
