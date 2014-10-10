fb58ef3d20d393217bf8b763dfa55e9bacf72cd0
Chris Loew <cloew123@gmail.com>
2014-10-10 15:33:43 -0600

    Setup for traditional game-play

--- src/Game/Decks/decks.py
+++ src/Game/Decks/decks.py
@@ -1,8 +1,8 @@
 from Game.Decks.deck_factory import DeckFactory
 
 StartingDeckInitializer = DeckFactory.loadDeck("Starting")
-# MainDeckInitializer = DeckFactory.loadDeck("Deck 1")
-MainDeckInitializer = DeckFactory.loadDeck("Test")
+MainDeckInitializer = DeckFactory.loadDeck("Deck 1")
+# MainDeckInitializer = DeckFactory.loadDeck("Test")
 KickDeckInitializer = DeckFactory.loadDeck("Kick")
 WeaknessDeckInitializer = DeckFactory.loadDeck("Weakness")
 SuperVillainDeckInitializer = DeckFactory.loadDeck("Super Villains")
