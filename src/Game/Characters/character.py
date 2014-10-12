e0d499f00a2283fef270db9116c6a4e003c05bb1
Chris Loew <cloew123@gmail.com>
2014-10-12 15:19:16 -0600

    Character can now load if it can go first

--- src/Game/Characters/character.py
+++ src/Game/Characters/character.py
@@ -2,10 +2,10 @@
 class Character:
     """ Represents a Player Character """
     
-    def __init__(self, name, triggers=[], activatable=None, image="batman.jpg"):
+    def __init__(self, name, triggers=[], activatable=None, goesFirst=False, image="batman.jpg"):
         """ Initialize the Character """
         self.name = name
-        self.goesFirst = False
+        self.goesFirst = goesFirst
         self.activatableEffect = activatable
         self.triggerEffects = triggers
         self.image = image
