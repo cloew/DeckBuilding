9c7efe34a32b1d788101328cecb4575968b69798
Chris Loew <cloew123@gmail.com>
2014-10-12 15:12:54 -0600

    Players are now placed in order based on whether they specify they can go first

--- src/Game/Characters/character.py
+++ src/Game/Characters/character.py
@@ -5,6 +5,7 @@ class Character:
     def __init__(self, name, triggers=[], activatable=None, image="batman.jpg"):
         """ Initialize the Character """
         self.name = name
+        self.goesFirst = False
         self.activatableEffect = activatable
         self.triggerEffects = triggers
         self.image = image
