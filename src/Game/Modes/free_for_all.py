c075fc5cae544f5889d0227eae7b7a253e4510ba
Chris Loew <cloew123@gmail.com>
2014-10-20 12:48:02 -0600

    Added Game Roles

--- src/Game/Modes/free_for_all.py
+++ src/Game/Modes/free_for_all.py
@@ -1,18 +1,30 @@
 from Game.game import Game
 from Game.Decks.deck_factory import DeckFactory
+from Game.Decks.deck_roles import MAIN, KICK, WEAKNESS, SUPERVILLAIN, STARTER
 
 import random
 
 class FreeForAll:
     """ Represents the Free For All Game Mode """
+    
+    def __init__(self):
+        """ Initialize the Free For All Game Mode """
+        requiredRoles = [MAIN, KICK, WEAKNESS, SUPERVILLAIN]
+        self.potentialDecks = {role:DeckFactory.findDeckIdsToFillRole(role) for role in requiredRoles}
+        
+        
+        self.mainDeckId = self.potentialDecks[MAIN][0]
+        self.kickDeckId = self.potentialDecks[KICK][0]
+        self.weaknessDeckId = self.potentialDecks[WEAKNESS][0]
+        self.supervillainDeckId = self.potentialDecks[SUPERVILLAIN][0]
         
     def buildGame(self, lobby):
         """ Build the Game """
         players = self.getGamePlayers(lobby)
-        return Game(players, mainDeck=DeckFactory.load("Deck 1 - Main").loadDeck(),
-                             kickDeck=DeckFactory.load("Deck 1 - Kick").loadDeck(),
-                             weaknessDeck=DeckFactory.load("Deck 1 - Weakness").loadDeck(),
-                             superVillainDeck=DeckFactory.load("Deck 1 - Super Villains").loadDeck())
+        return Game(players, mainDeck=DeckFactory.load(self.mainDeckId).loadDeck(),
+                             kickDeck=DeckFactory.load(self.kickDeckId).loadDeck(),
+                             weaknessDeck=DeckFactory.load(self.weaknessDeckId).loadDeck(),
+                             superVillainDeck=DeckFactory.load(self.supervillainDeckId).loadDeck())
         
     def getGamePlayers(self, lobby):
         """ Return the Game Players in their proper order """
