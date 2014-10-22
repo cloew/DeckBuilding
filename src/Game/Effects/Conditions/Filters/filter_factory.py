c9608ff18d0a4bf56c98befccfbb271ec8fcd36a
Chris Loew <cloew123@gmail.com>
2014-10-22 11:01:46 -0600

    Added Saint Walker

--- src/Game/Effects/Conditions/Filters/filter_factory.py
+++ src/Game/Effects/Conditions/Filters/filter_factory.py
@@ -10,5 +10,5 @@ from kao_factory.Parameter.complex_parameter import ComplexParameter
 from kao_factory.Parameter.primitive_parameter import PrimitiveParameter
 
 FilterFactory = TypedFactory('type', {"COMPARISON":Factory(ComparisonFilter, [PrimitiveParameter("source"), ComplexParameter("criteria", CriteriaFactory.load)]),
-                                      "UNIQUE":Factory(UniqueFilter, [PrimitiveParameter("field"), PrimitiveParameter("sourceType")])})
+                                      "UNIQUE":Factory(UniqueFilter, [PrimitiveParameter("field"), PrimitiveParameter("source")])})
 FilterFactory.addFactory("INTERSECTION", Factory(IntersectionFilter, [ComplexParameter("filters", FilterFactory.loadAll)]))
