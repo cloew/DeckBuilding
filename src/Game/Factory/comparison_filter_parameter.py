3c287728ef6dc0e41bb8a754fe8b761c494a6631
Chris Loew <cloew123@gmail.com>
2014-10-28 11:49:03 -0600

    Tweaked Comparison Filter Parameter so it does not have dead code

--- src/Game/Factory/comparison_filter_parameter.py
+++ src/Game/Factory/comparison_filter_parameter.py
@@ -1,5 +1,4 @@
 from Game.Effects.Conditions.Filters.filter_factory import FilterFactory
-from Game.Effects.Conditions.Filters.intersection_filter import IntersectionFilter
 from kao_factory.Parameter.parameter import Parameter
 
 class ComparisonFilterParameter(Parameter):
@@ -12,17 +11,7 @@ class ComparisonFilterParameter(Parameter):
     
     def __getvalue__(self, data):
         """ Build the Filter """
-        if "criterion" not in data:
-            criterion = [data["criteria"]]
-            
-        filters = []
-        for criteria in criterion:
-            filterJson = {"criteria":data["criteria"]}
-            filterJson["source"] = data["source"]
-            filterJson["type"] = "COMPARISON"
-            filters.append(FilterFactory.load(filterJson))
-            
-        if len(filters) == 1:
-            return filters[0]
-        else:
-            return IntersectionFilter(filters)
+        filterJson = {"criteria":data["criteria"]}
+        filterJson["source"] = data["source"]
+        filterJson["type"] = "COMPARISON"
+        return FilterFactory.load(filterJson))
