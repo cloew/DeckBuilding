1d7f5d9a70c7220bdf8849e6f36f62ca9c260e3f
Chris Loew <cloew123@gmail.com>
2014-10-29 13:33:09 -0600

    Pick Card Controller now uses the Game Command base

--- src/Server/Game/Controller/pick_card_controller.py
+++ src/Server/Game/Controller/pick_card_controller.py
@@ -1,15 +1,16 @@
 from Game.Commands.Responses.pick_card import PickCard
-from Server.Game.games import games
 
-from kao_flask.controllers.json_controller import JSONController
+from Server.Game.Controller.game_command_controller import GameCommandController
 
-class PickCardController(JSONController):
+class PickCardController(GameCommandController):
     """ Controller to pick a card """
-    
-    def performWithJSON(self, gameId, playerId):
-        game = games[gameId]
-        cardIndices = self.json['indices']
-        cards = [game.game.currentTurn.request.cards[cardIndex] for cardIndex in cardIndices]
         
-        PickCard(cards, game.game.currentTurn).perform()
-        return game.toJSONForPlayer(playerId)
+    def buildCommand(self, player, game, json):
+        """ Build the Command to try and perform """
+        cardIndices = self.json['indices']
+        
+        card = None
+        if game.currentTurn.request is not None:
+            cards = [game.currentTurn.request.cards[cardIndex] for cardIndex in cardIndices if cardIndex < len(game.currentTurn.request.cards)]
+            command = PickCard(cards, game.currentTurn)
+        return command
