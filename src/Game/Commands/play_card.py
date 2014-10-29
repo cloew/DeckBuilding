62aaf142a7215e455a9e43af520084023f87c10a
Chris Loew <cloew123@gmail.com>
2014-10-29 12:17:22 -0600

    Play Card Command now specifies its requirements

--- src/Game/Commands/play_card.py
+++ src/Game/Commands/play_card.py
@@ -1,11 +1,16 @@
+from command import Command
 
-class PlayCard:
+from Game.Commands.Requirements.current_player import CurrentPlayer
+from Game.Commands.Requirements.no_request import NoRequest
+
+class PlayCard(Command):
     """ Represents a Command to play a card """
     
     def __init__(self, card, owner):
         """ Initialize the Play Card Command """
         self.card = card
         self.owner = owner
+        Command.__init__(self, [CurrentPlayer(), NoRequest()])
         
     def perform(self):
         """ Perform the command """
