da4b9f855b3f7de9d1b70f50e54d7999ff7ebb7a
Chris Loew <cloew123@gmail.com>
2014-10-29 12:13:22 -0600

    End Turn command now specifies its requirements

--- src/Game/Commands/end_turn.py
+++ src/Game/Commands/end_turn.py
@@ -1,10 +1,15 @@
+from command import Command
 
-class EndTurn:
+from Game.Commands.Requirements.current_player import CurrentPlayer
+from Game.Commands.Requirements.no_request import NoRequest
+
+class EndTurn(Command):
     """ Represents a Command to end the turn """
     
     def __init__(self, game):
         """ Initialize the Command """
         self.game = game
+        Command.__init__(self, [CurrentPlayer(), NoRequest()])
         
     def perform(self):
         """ Perform the command """
