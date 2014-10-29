eb9b5f82284a560f562238ff3f87301c8b3c2a86
Chris Loew <cloew123@gmail.com>
2014-10-29 13:32:09 -0600

    Pick Card now specifies its requirements

--- src/Game/Commands/Responses/pick_card.py
+++ src/Game/Commands/Responses/pick_card.py
@@ -1,11 +1,15 @@
+from Game.Commands.command import Command
 
-class PickCard:
+from Game.Commands.Requirements.request_target import RequestTarget
+
+class PickCard(Command):
     """ Represents a Command to pick a card """
     
     def __init__(self, cards, owner):
         """ Initialize the Pick Card Command """
         self.cards = cards
         self.owner = owner
+        Command.__init__(self, [RequestTarget()])
         
     def perform(self):
         """ Perform the command """
