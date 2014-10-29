787b2aaf3dc3a44b0cca40d21bae6a63f6d7cf5d
Chris Loew <cloew123@gmail.com>
2014-10-29 12:11:54 -0600

    Activate COntroller now uses the Game COmmand base

--- src/Server/Game/Controller/activate_card_controller.py
+++ src/Server/Game/Controller/activate_card_controller.py
@@ -2,20 +2,21 @@ from Game.Commands.activate_card import ActivateCard
 from Game.Sources.source_factory import SourceFactory
 from Server.Game.games import games
 
-from kao_flask.controllers.json_controller import JSONController
+from Server.Game.Controller.game_command_controller import GameCommandController
 
-class ActivateCardController(JSONController):
+class ActivateCardController(GameCommandController):
     """ Controller to activate a card """
-    
-    def performWithJSON(self, gameId, playerId):
-        game = games[gameId]
-        player = game.players[playerId]
+        
+    def buildCommand(self, player, game, json):
+        """ Build the Command to try and perform """
         cardIndex = self.json['index']
         sourceType = self.json['source']
         
-        card = None
-        source = SourceFactory.getSource(sourceType, game.game, player=player)
-        card = source[cardIndex]
+        command = None
+        source = SourceFactory.getSource(sourceType, game, player=player)
         
-        game.game.currentTurn.perform(ActivateCard(card, game.game.currentTurn))
-        return game.toJSONForPlayer(playerId)
+        if cardIndex < len(source):
+            card = source[cardIndex]
+            command = ActivateCard(card, game.currentTurn)
+        
+        return command
