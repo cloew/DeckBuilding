bbd0aca5d18037056f6a65a69bb4766c1a688b03
Chris Loew <cloew123@gmail.com>
2014-10-29 12:08:35 -0600

    Activate Card Command now specifies its requirements

--- src/Game/Commands/activate_card.py
+++ src/Game/Commands/activate_card.py
@@ -1,12 +1,18 @@
+from command import Command
+
+from Game.Commands.Requirements.current_player import CurrentPlayer
+from Game.Commands.Requirements.no_request import NoRequest
+
 from Game.Effects.game_contexts import PlayerContext
 
-class ActivateCard:
+class ActivateCard(Command):
     """ Represents a command to activate a card """
     
     def __init__(self, card, owner):
         """ Initialize the Activate Card Command """
         self.card = card
         self.owner = owner
+        Command.__init__(self, [CurrentPlayer(), NoRequest()])
         
     def perform(self):
         """ Perform the command """
