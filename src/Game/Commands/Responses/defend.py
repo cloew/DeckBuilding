28b8573b931288aa65bcd8702694c53ca7cfc23b
Chris Loew <cloew123@gmail.com>
2014-10-29 13:28:40 -0600

    Defned now specifies its requirements

--- src/Game/Commands/Responses/defend.py
+++ src/Game/Commands/Responses/defend.py
@@ -1,11 +1,15 @@
+from Game.Commands.command import Command
 
-class Defend:
+from Game.Commands.Requirements.request_target import RequestTarget
+
+class Defend(Command):
     """ Represents a Command to defend """
     
     def __init__(self, card, owner):
         """ Initialize the Defend Command """
         self.card = card
         self.owner = owner
+        Command.__init__(self, [RequestTarget()])
         
     def perform(self):
         """ Perform the command """
