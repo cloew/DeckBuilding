8ae792dae5e8cf02b38935008f5150bd9c51a1ff
Chris Loew <cloew123@gmail.com>
2014-10-30 10:13:05 -0600

    Can now display all cards in your deck to see what gave you points

--- src/Server/Game/player_results_wrapper.py
+++ src/Server/Game/player_results_wrapper.py
@@ -14,8 +14,15 @@ class PlayerResultsWrapper:
         """ Return the Player as a JSON Dictionary """
         cards = {}
         cardsByType = {str(cardType):list(cardsForType) for cardType, cardsForType in groupby(sorted(self.player.deck, key=lambda card: card.cardType), key=lambda card: card.cardType)}
+        
+        cardTypes = []
         for cardType in cardsByType:
+            cardTypes.append(cardType)
             cardsForType = cardsByType[cardType]
+            
             cardsSortedByCost = sorted(cardsForType, key=lambda card: card.calculatePoints(game))
             cards[cardType] = {str(points):GetCardListJSON(cardsForPoints, game) for points, cardsForPoints in groupby(cardsSortedByCost, key=lambda card: card.calculatePoints(game))}
+            cards[cardType]['pointValues'] = list(cards[cardType].keys())
+            
+        cards['cardTypes'] = cardTypes
         return {'points':self.player.calculatePoints(game), 'name':self.player.name, 'isYou':self.isYou, 'cards':cards}
