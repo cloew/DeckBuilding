c953df5dcf2a3da3fae4b8d5e7591a97eeecd286
Chris Loew <cloew123@gmail.com>
2014-11-03 09:32:47 -0700

    Cleaned up the PerMatch Effect Factory so it can load without a source type

--- src/Game/Effects/effect_factory.py
+++ src/Game/Effects/effect_factory.py
@@ -119,7 +119,7 @@ EffectFactory.addFactory("COLLECT_CARDS", Factory(CollectCards, [ComplexParamete
 EffectFactory.addFactory("FILTER", Factory(Filter, [PrimitiveParameter("source"), FilterParameter(), ComplexParameter("then", EffectFactory.loadAll)]))
 EffectFactory.addFactory("LOOK_AT_TOP", Factory(LookAtTop, [PrimitiveParameter("source"), ComplexParameter("then", EffectFactory.loadAll), PrimitiveParameter("number", optional=True)]))
 EffectFactory.addFactory("PER_FOE", Factory(PerFoe, [ComplexParameter("effects", EffectFactory.loadAll)]))
-EffectFactory.addFactory("PER_MATCH", Factory(PerMatch, [PrimitiveParameter("source"), ComplexParameter("effects", EffectFactory.loadAll), FilterParameter(optional=True)]))
+EffectFactory.addFactory("PER_MATCH", Factory(PerMatch, [PrimitiveParameter("source", optional=True), ComplexParameter("effects", EffectFactory.loadAll), FilterParameter(optional=True)]))
 EffectFactory.addFactory("PICK_CARDS", Factory(PickCards, [PrimitiveParameter("sources"), PrimitiveParameter("number"), PrimitiveParameter("to"),
                                                            ComplexParameter("then", EffectFactory.loadAll), ComplexParameter("criteria", CriteriaFactory.loadAll, optional=True),
                                                            ComplexParameter("leftoverCardEffects", EffectFactory.loadAll, optional=True, default=[])]))
