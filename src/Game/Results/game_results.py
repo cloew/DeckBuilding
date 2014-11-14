1f05f4ced14764bcb1ef2ca2187812e8174c3f44
Chris Loew <cloew123@gmail.com>
2014-11-13 18:12:39 -0700

    Added game results class

---
+++ src/Game/Results/game_results.py
@@ -0,0 +1,7 @@
+
+class GameResults:
+    """ Represents the results for a game """
+    
+    def __init__(self, playerResults):
+        """ Initialize the Game Results with the results for each player """
+        self.playerResults = playerResults
