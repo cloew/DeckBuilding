18b8d7167fff6efbfecc715b6e907d042e502336
Chris Loew <cloew123@gmail.com>
2015-02-09 23:32:15 -0700

    Removed extremely outdated start game controller

--- src/Server/Game/Controller/start_game_controller.py
+++
@@ -1,9 +0,0 @@
-from Server.Game.games import games
-from kao_flask.controllers.json_controller import JSONController
-
-class StartGameController(JSONController):
-    """ Controller to handle starting a new Game via JSON """
-    
-    def performWithJSON(self, json=None):
-        id = StartNewGame()
-        return games[id].toJSON(), 201
