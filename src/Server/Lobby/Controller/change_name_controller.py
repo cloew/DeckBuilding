fefa153b9f7415e41c7d965faa58261ccbe9019e
Chris Loew <cloew123@gmail.com>
2015-02-09 21:05:20 -0700

    Updated the Change Name Controller to use the Json Factory

--- src/Server/Lobby/Controller/change_name_controller.py
+++ src/Server/Lobby/Controller/change_name_controller.py
@@ -1,11 +1,11 @@
-from Server.Lobby.lobbies import lobbies
+from Server.helpers.json_factory import jsonFactory
+from Server.Lobby.lobbies import lobbies, lobbyIdToPlayers
 from kao_flask.controllers.json_controller import JSONController
 
 class ChangeNameController(JSONController):
     """ Controller to handle changing a player's name via JSON """
     
     def performWithJSON(self, lobbyId, playerId, json=None):
-        lobby = lobbies[lobbyId]
-        player = lobby.players[playerId]
-        player.setName(json['name'])
-        return lobbies[lobbyId].toJSONForPlayer(playerId), 201
+        currentPlayer=lobbyIdToPlayers[lobbyId][playerId]
+        currentPlayer.setName(json['name'])
+        return jsonFactory.toJson(lobbies[lobbyId], playerId=playerId, currentPlayer=currentPlayer), 201
