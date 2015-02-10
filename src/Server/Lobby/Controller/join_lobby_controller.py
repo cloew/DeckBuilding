de72f6cc4b73e603fe48d058defd0d11f7772e66
Chris Loew <cloew123@gmail.com>
2015-02-09 21:09:42 -0700

    Updated Join Lobby Controller to use the Json Factory

--- src/Server/Lobby/Controller/join_lobby_controller.py
+++ src/Server/Lobby/Controller/join_lobby_controller.py
@@ -1,10 +1,10 @@
-from Server.Lobby.lobbies import lobbies
+from Server.helpers.json_factory import jsonFactory
+from Server.Lobby.lobbies import AddPlayerToLobby
 from kao_flask.controllers.json_controller import JSONController
 
 class JoinLobbyController(JSONController):
     """ Controller to handle creating a new Game Lobby via JSON """
     
     def performWithJSON(self, lobbyId, json=None):
-        lobby = lobbies[lobbyId]
-        playerId = lobby.addPlayer()
-        return {'playerId':playerId, 'lobbyId':lobby.id}, 201
+        playerId = AddPlayerToLobby(lobbyId)
+        return {'playerId':playerId, 'lobbyId':lobbyId}, 201
