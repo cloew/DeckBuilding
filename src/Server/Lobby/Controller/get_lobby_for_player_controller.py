9d47e9d6b921338e50f9df83ffaffdaca535a04a
Chris Loew <cloew123@gmail.com>
2015-02-09 19:00:52 -0700

    Updated the get lobby for player controller

--- src/Server/Lobby/Controller/get_lobby_for_player_controller.py
+++ src/Server/Lobby/Controller/get_lobby_for_player_controller.py
@@ -1,8 +1,9 @@
-from Server.Lobby.lobbies import lobbies
+from Server.helpers.json_factory import jsonFactory
+from Server.Lobby.lobbies import lobbies, lobbyIdToPlayers
 from kao_flask.controllers.json_controller import JSONController
 
 class GetLobbyForPlayerController(JSONController):
     """ Controller to return basic lobby details """
     
     def performWithJSON(self, lobbyId, playerId, json=None):
-        return lobbies[lobbyId].toJSONForPlayer(playerId)
+        return jsonFactory.toJson(lobbies[lobbyId], playerId=playerId, currentPlayer=lobbyIdToPlayers[lobbyId][playerId])
