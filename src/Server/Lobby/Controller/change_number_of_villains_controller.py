d8ad72ac3c7c7e6205c77abd914ee8349666623b
Chris Loew <cloew123@gmail.com>
2015-02-09 21:07:58 -0700

    Change Number of Villains COntroller now uses the Json Factory

--- src/Server/Lobby/Controller/change_number_of_villains_controller.py
+++ src/Server/Lobby/Controller/change_number_of_villains_controller.py
@@ -1,4 +1,5 @@
-from Server.Lobby.lobbies import lobbies
+from Server.helpers.json_factory import jsonFactory
+from Server.Lobby.lobbies import lobbies, lobbyIdToPlayers
 from kao_flask.controllers.json_controller import JSONController
 
 class ChangeNumberOfVillainsController(JSONController):
@@ -6,6 +7,6 @@ class ChangeNumberOfVillainsController(JSONController):
     
     def performWithJSON(self, lobbyId, playerId, json=None):
         lobby = lobbies[lobbyId]
-        player = lobby.players[playerId]
-        lobby.lobby.gameMode.setNumberOfVillains(json['index'])
-        return lobbies[lobbyId].toJSONForPlayer(playerId), 201
+        currentPlayer=lobbyIdToPlayers[lobbyId][playerId]
+        lobby.gameMode.setNumberOfVillains(json['index'])
+        return jsonFactory.toJson(lobbies[lobbyId], playerId=playerId, currentPlayer=currentPlayer), 201
